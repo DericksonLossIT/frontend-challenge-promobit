835a8db4aa8119f46c77bc61de68f21c0d5c2ffb
Derickson Loss <dericksonlossit@gmail.com>
2022-02-05 18:12:33 -0300

    added semantic html insted of div

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import Navbar from "./components/Navbar";
-import { getPopularMovies } from "./utils/getPopularMovies";
-
 function App() {
   return (
     <div className="App">
-    <Navbar />
-      <button type='button' onClick={() => getPopularMovies()} >
-        getPopularMovies
-        
-      </button>
+      
     </div>
   );
 }
